02bf92e90234f4bb608aba27a4f98103f5bb9b5a
SashaGonch228 <sashaandsergey20@gmail.com>
2026-06-04 01:47:45 +0500

    Update getGeniusLink

--- src/modules/ui/dom.ts
+++ src/modules/ui/dom.ts
@@ -1307,8 +1307,8 @@ export function injectSongAttributes(title: string, artist: string): void {
  * @param artist - Artist name
  */
 function getGeniusLink(song: string, artist: string): string {
-  const searchQuery = encodeURIComponent(`${artist.trim()} - ${song.trim()}`);
-  return `https://genius.com/search?q=${searchQuery}`;
+    const query = encodeURIComponent(`site:genius.com ${artist.trim()} ${song.trim()}`);
+    return `https://duckduckgo.com/?q=!ducky+site:genius.com+${query}`;
 }
 
 export function setExtraHeight() {
